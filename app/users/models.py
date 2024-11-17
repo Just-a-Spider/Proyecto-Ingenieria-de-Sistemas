e1f4e7467bff19658000bc5603d22df7e9d2315d
Daniel <2022160045@udh.edu.pe>
2024-11-16 20:56:38 -0500

    cambios usuarios

--- app/users/models.py
+++ app/users/models.py
@@ -1,7 +1,7 @@
 from app import db
 from flask_login import UserMixin
 from datetime import datetime
-#models para usuarios
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False, unique=True)
