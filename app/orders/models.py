b548d618b4e808c9ffe1973accecfd388d289ee1
Leonardo-Jaren <2022110517@udh.edu.pe>
2024-11-30 19:56:30 -0500

    Eliminación de estilos CSS, implementación de autenticación en las órdenes y adición de plantillas HTML para clientes y reembolsos.

--- app/orders/models.py
+++ app/orders/models.py
@@ -5,16 +5,16 @@ class Client(db.Model):
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
     phone = db.Column(db.String(9), nullable=False)
-    registration_date = db.Column(db.DateTime, nullable=False)
+    registration_date = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
 
     # Relationships
     orders = db.relationship('Order', backref='client', lazy=True)
 
 class Order(db.Model):
-    uuid = db.Column(db.String(36), primary_key=True)
-    order_date = db.Column(db.DateTime, nullable=False)
-    status = db.Column(db.String(10), nullable=False)  # The status can only be: 'Pendiente', 'Enviado', 'Cancelado'
-    total = db.Column(db.Float, nullable=False)
+    uuid = db.Column(db.String(36), primary_key=True, default=db.func.uuid())
+    order_date = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
+    status = db.Column(db.String(10), nullable=False, default='Pendiente')  # Default status is 'Pendiente'
+    total = db.Column(db.Float, nullable=False, default=0.0)  # Default total is 0.0
     payment_method = db.Column(db.String(10), nullable=False)  # The payment method can only be: 'Efectivo', 'Tarjeta de crédito', 'Tarjeta de débito'
 
     # Foreign keys
@@ -24,7 +24,7 @@ class Order(db.Model):
     order_details = db.relationship('OrderDetail', backref='order', lazy=True)
 
 class OrderDetail(db.Model):
-    uuid = db.Column(db.String(36), primary_key=True)
+    uuid = db.Column(db.String(36), primary_key=True, default=db.func.uuid())
     quantity = db.Column(db.Integer, nullable=False)
     subtotal = db.Column(db.Float, nullable=False)
 
