20beb41db33e81e319acd1870496ff41c9184792
johan-nestor <2022110505@udh.edu.pe>
2024-12-01 19:52:46 -0500

    Cambios y diseño para productos

--- app/products/views.py
+++ app/products/views.py
@@ -1,11 +1,12 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, redirect, url_for, request
 from app.products import products_bp
 from app.products.models import Product
 from app.products.forms import CreateProductForm
 from app import db
 
-@products_bp.route('/products', methods=['GET', 'POST'])
-def products():
+@products_bp.route('/productos', methods=['GET', 'POST'])
+def productos():
+    message = request.args.get('message', None)
     form = CreateProductForm()
     if form.validate_on_submit():
         new_product = Product(
@@ -16,7 +17,19 @@ def products():
         )
         db.session.add(new_product)
         db.session.commit()
-        return redirect(url_for('products.products'))
 
     products = Product.query.all()
-    return render_template('products.html', products=products, form=form)
+    return render_template(
+        'products.html', 
+        products=products, 
+        form=form,
+        message=message
+    )
+
+@products_bp.route('/producto/<int:id>', methods=['POST'])
+def actualizar_stock(id):
+    stock = request.form.get('stock')
+    product = Product.query.get_or_404(id)
+    product.stock = stock
+    db.session.commit()
+    return redirect(url_for('products.productos', message='Stock actualizado correctamente'))
